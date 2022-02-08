e5fd4d15a13fed06e6b363f7f3b54e6dc0506b73
Ola793 <67291449+Ola793@users.noreply.github.com>
2022-02-08 13:03:49 +0200

    Update spinal-tap-case.js

--- spinal-tap-case.js
+++ spinal-tap-case.js
@@ -1,3 +1,5 @@
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
 function spinalCase(str) {
   return str.replace(/([A-Z])|(-)|(_)/g, ' $1').trim().toLowerCase().replace(/\s\s+/g,' ').split(' ').join('-');
 }
